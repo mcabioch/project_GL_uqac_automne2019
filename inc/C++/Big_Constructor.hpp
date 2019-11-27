b409011222a53b4d6e44e83d8fd3fd2bfb8c9450
Mathias Cabioch-Delalande <mathias.cabiochdelalande@gmail.com>
2019-11-27 10:40:38 -0500

    Let only project files for BetterCodeHub analyze

--- inc/C++/Big_Constructor.hpp
+++
@@ -1,7 +0,0 @@
-namespace mcd {
-	/*! \brief	Construct a Big with any type */
-	template<typename T, class = std::enable_if_t<std::is_arithmetic<T>::value> >
-	Big::Big(T val) :
-		Big(toBigVal(val))
-	{}
-}
